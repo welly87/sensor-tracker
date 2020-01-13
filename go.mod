7b0786cd4200318dab4e730afd140813b502e0fa
Welly Tambunan <if05041@gmail.com>
2020-01-13 08:29:43 +0700

    add sender and receiver

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/welly87/sensor-tracker
+
+go 1.13
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.2.0
+	github.com/shirou/gopsutil v2.19.12+incompatible
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	golang.org/x/sys v0.0.0-20200107162124-548cf772de50 // indirect
+)
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/shirou/gopsutil/mem"
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+func main() {
+	v, _ := mem.VirtualMemory()
+
+    // almost every return value is a struct
+    fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
+
+    // convert to JSON. String() is also implemented
+	fmt.Println(v)
+	
+	opts := MQTT.NewClientOptions()
+	opts.AddBroker("ws://localhost:1883")
+	opts.SetClientID("MacBookPro2")
+
+	client := MQTT.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	fmt.Println("Sample Publisher Started")
+	for i := 0; i < 5; i++ {
+		fmt.Println("---- doing publish ----")
+		token := client.Publish("something", byte(0), false, "this is the payload")	
+		token.Wait()
+	}
+
+	client.Disconnect(250)
+	fmt.Println("Sample Publisher Disconnected")
+}
